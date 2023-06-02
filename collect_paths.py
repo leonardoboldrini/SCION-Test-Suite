54c60c4943912c844dd79b92ddcdb2045c0e211a
MrR0b0t14 <s297925@studenti.polito.it>
2023-06-02 17:44:13 +0200

    Finishing showpaths and insertion in DB + conversion of hop predicates

--- collect_paths.py
+++ collect_paths.py
@@ -3,13 +3,38 @@ import sys # serve per accedere ad argv
 import re
 from pymongo import MongoClient
 
+def convert_hop_predicates(old_hop_predicates):
+    new_hop_predicates = ""
+    outgoing_interface = ""
+    incoming_interface = ""
 
-def path_info_building(server, paths_to_be_inserted):
+    for hop_number, hop_predicate in enumerate(old_hop_predicates):
+        if('>' in hop_predicate):
+            interfaces = hop_predicate.split(">")
+            outgoing_interface = interfaces[0]
+            incoming_interface = interfaces[1]
+            new_hop_predicates += "," + outgoing_interface + " "
+        else:
+            new_hop_predicates += hop_predicate + "#"
+            if hop_number == 0:
+                new_hop_predicates += "0"
+            else:
+                new_hop_predicates += incoming_interface
+    return new_hop_predicates
+
+def path_info_building(server):
+    paths_to_be_inserted = []
+    scion_addr_cmd = "scion address"
+    scion_addr_proc = subprocess.Popen(scion_addr_cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    source_addr = scion_addr_proc.stdout.readline()
+    source_addr = source_addr.decode('utf-8').rstrip()
+
     #execute scion showpaths command
     cmd = f"scion showpaths {server} --extended"
 
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
+    
     # Read the output of the command and store it in a list
     output = []
     dirty_path_info = []
@@ -40,20 +65,21 @@ def path_info_building(server, paths_to_be_inserted):
     # Now I need to change the format of the hops field
     for path in dirty_path_info:
         old_hop_field = path["Hops"]
-        #TODO: operations to reformat the old_hop_field
-        new_hop_field = []
+        new_hop_field = convert_hop_predicates(old_hop_field[1:-1])
 
         new_path = {
             "_id": path["Path_ID"],
             "hop_predicates": new_hop_field,
             "MTU": path["MTU"],
-            "expected_min_atency": path["Latency"],
+            "expected_min_latency": path["Latency"],
             "active": path["Status"],
             "destination_address": server.source_address,
-            "source_address": "My address", #TODO: this a placeholder
+            "source_address": source_addr,
         }
 
         paths_to_be_inserted.append(new_path)
+
+    return paths_to_be_inserted
     
 def insert_paths(paths_to_be_inserted):
     # Access the desired collection
@@ -72,9 +98,8 @@ if __name__ == "__main__":
     
     # Access the desired database
     db = client['scionStatsDB']
-    available_servers = db['availableServers']
-    paths_to_be_inserted = []
-
+    available_servers = db['availableServers'].find()
+    
     for server in available_servers:
-        path_info_building(server, paths_to_be_inserted)
+        paths_to_be_inserted = path_info_building(server, paths_to_be_inserted)
         insert_paths(paths_to_be_inserted)
