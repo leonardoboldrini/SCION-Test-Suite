fe41a0b1b6aa02fe2fcf3a91c5a7c3bc02c4eb0f
MrR0b0t14 <s297925@studenti.polito.it>
2023-08-02 12:05:07 +0200

    BW temporary tests

--- Temp/test_suite_temp.sh
+++ Temp/test_suite_temp.sh
@@ -9,9 +9,9 @@ args="$@"
 
 # Collect the paths
 echo "Collecting all the paths..."
-python3 collect_paths.py
+python3 ../collect_paths.py
 
 echo "Running the bw test suite..."
-python3 Temp/bw_temp_test.py -n $1
+python3 bw_temp_test.py -n $1
 exit 0
 
